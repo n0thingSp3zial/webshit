8fc30037cb917b9b3058f5c425cb1f59aff7bcdb
n0thingSp3zial <93796276+n0thingSp3zial@users.noreply.github.com>
2023-02-03 00:56:59 +0300

    v1.41

--- app.js
+++ app.js
@@ -15,12 +15,34 @@ button.addEventListener("click", (e) => {
         rootEl.style.setProperty('--clr', '#9bff1e')
         button.innerText = 'start'
         rootEl.style.setProperty('--offset', '0px')
-        chart.style.display = 'block'
+        chart.style.display = 'none'
     } else {
         rootEl.style.setProperty('--clr', '#ff1867')
         button.innerText = 'stop'
         rootEl.style.setProperty('--offset', Math.floor(document.documentElement.scrollHeight / 4) + 'px')
-        chart.style.display = 'none'
+        chart.style.display = 'block'
     }
 })
 
+function _(selector) {
+    return document.querySelector(selector)
+}
+let x = _('#x')
+let y = _('#y')
+let z = _('#z')
+
+if (window.DeviceMotionEvent == undefined) {
+    alert('no accelerometer')
+} else {
+    window.addEventListener("devicemotion", accelerometerUpdate, true)
+}
+
+function accelerometerUpdate(e) {
+    let acc = e.accelerationIncludingGravity
+    let aX = acc.x
+    let aY = acc.y
+    let aZ = acc.z
+    x.innerText = aX.toFixed(2)
+    y.innerText = aY.toFixed(2)
+    z.innerText = aZ.toFixed(2)
+}
